cd95fef9a8b6019d2dab39249c40a85a5d2f29e1
Jfeurich <jayh4x0r@gmail.com>
2014-12-11 17:06:36 +0100

    category/context

--- src/Model/Category.java
+++ src/Model/Category.java
@@ -1,10 +1,17 @@
 package Model;
 
 public class Category implements Context{
+	private String name;
 	private String description;
+	private Category category;
 
 	public Category( String desc){
 		description = desc;
+	}
+	
+	public Category(String desc,String name){
+		name = this.name;
+		desc = this.description;
 	}
 
 	public String getDescription(){
@@ -18,4 +25,20 @@ public class Category implements Context{
 	/*public Category getCategory(){
 		return Category;
 	}*/
+
+	public Context getContext() {
+		return category;
+	}
+
+	public void setContext(Category context) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
