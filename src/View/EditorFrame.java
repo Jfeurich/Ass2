de1b7625954bb6c4c52afb8a42c23a724b566e5b
Jfeurich <jayh4x0r@gmail.com>
2014-12-14 09:58:54 +0100

    EditorFrame geupdate

--- src/View/EditorFrame.java
+++ src/View/EditorFrame.java
@@ -10,7 +10,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import org.apache.commons.codec.binary.Base64;
+import it.sauronsoftware.base64.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
