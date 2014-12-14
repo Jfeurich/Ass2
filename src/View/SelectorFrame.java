6b00f5832792f25d52f4e9d944135bd375fd9b72
Jfeurich <jayh4x0r@gmail.com>
2014-12-14 10:43:41 +0100

    All i can see is changes

--- src/View/SelectorFrame.java
+++ src/View/SelectorFrame.java
@@ -1,6 +1,7 @@
 package View;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 import javax.swing.border.*;
@@ -8,6 +9,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import Model.Category;
+import Model.Pattern;
 
 public class SelectorFrame extends MyFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
@@ -15,11 +17,16 @@ public class SelectorFrame extends MyFrame implements ActionListener{
 	private JComboBox categoryList, purposeList, scopeList;
 	
 	
-	String[]categoryString = {"By Purpose", "By Scope"};
-	String[]purposeString = {"Creational", "Structional", "Behavioral"};
-	String[]scopeString = {"Class", "Object"};
+	ArrayList<String> categoryString = new ArrayList<>();
+	ArrayList<String> purposeString = new ArrayList<>(); 
+	ArrayList<String> scopeString = new ArrayList<>();
 	
-	public SelectorFrame(){
+	public SelectorFrame(ArrayList<Pattern> p){
+		for(Pattern p1 : p){
+			categoryString.add("");
+			purposeString.add("");
+			scopeString.add("");
+		}
 		setLayout(new FlowLayout());
 		JPanel hulp1 = new JPanel();
 		add(hulp1);
@@ -27,7 +34,7 @@ public class SelectorFrame extends MyFrame implements ActionListener{
 		categoryLAB = new JLabel("Category");
 		hulp1.add(categoryLAB);
 		
-		categoryList = new JComboBox(categoryString);
+		categoryList = new JComboBox((ComboBoxModel) categoryString);
 		//categoryList.setSelectedIndex(1);
 		categoryList.addActionListener(this);
 		hulp1.add(categoryList);
@@ -39,13 +46,13 @@ public class SelectorFrame extends MyFrame implements ActionListener{
 		subCategoryLAB = new JLabel("subcategory");
 		hulp2.add(subCategoryLAB);
 		
-		purposeList = new JComboBox(purposeString);
+		purposeList = new JComboBox((ComboBoxModel) purposeString);
 		//purposeList.setSelectedIndex(1);
 		purposeList.addActionListener(this);
 		purposeList.setVisible(false);
 		hulp2.add(purposeList);
 		
-		scopeList = new JComboBox(scopeString);
+		scopeList = new JComboBox((ComboBoxModel) scopeString);
 		//scopeList.setSelectedIndex(2);
 		scopeList.addActionListener(this);
 		scopeList.setVisible(false);
